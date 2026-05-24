f404cf7c10e9a3ff5c64b0ec750f9cf794d83935
M Lintang Maulana Zulfan <lintangmaulanazulfan@gmail.com>
2026-05-24 16:25:24 +0700

    feat: implement i18n support with localization files and global type definitions

---
+++ apps/web/global.d.ts
@@ -0,0 +1,11 @@
+// Mengaktifkan strict typing untuk next-intl
+// Menggunakan Union Type (|) untuk ketiga bahasa. 
+// TypeScript hanya akan mengizinkan key yang ADA DI KETIGA FILE.
+// Jika ada key yang missing di salah satu file, maka akan menjadi error saat dicompile.
+type MessagesId = typeof import('./messages/id.json');
+type MessagesEn = typeof import('./messages/en.json');
+type MessagesZh = typeof import('./messages/zh.json');
+
+type Messages = MessagesId | MessagesEn | MessagesZh;
+
+declare interface IntlMessages extends Messages {}
