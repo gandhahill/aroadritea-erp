737f4e25877740390f9806b981b78e7970010915
M Lintang Maulana Zulfan <lintangmaulanazulfan@gmail.com>
2026-05-30 19:24:21 +0700

    chore: remove obsolete database setup and environment scripts

--- update_env.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's|^DATABASE_URL=.*|DATABASE_URL="postgresql://aroadritea:local_erp_s3cr3t_2026@127.0.0.1:5432/aroadritea_erp"|' /home/aroadritea/web/aroadritea.com/public_html/aroadritea-erp/.env
